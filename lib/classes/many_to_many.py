172d1fda0f056e3afceacc36eb4937fd7403aebc
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-10 14:42:27 -0400

    demo done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,110 @@
 class Coffee:
     def __init__(self, name):
+        #make sure to use the property setter, not self._name
         self.name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        #all the prices
+        # sum = 0
+        # for order in self.orders():
+        #     sum += order.price
+        total = sum([order.price for order in self.orders()])
+         #divide by number of orders
+        return total/self.num_orders()
+
+    @property 
+    def name(self):
+        return self._name 
+    
+    @name.setter 
+    def name(self, new_name):
+        #check if _name has already been created
+        if(hasattr(self, 'name')):
+            return 'no'
+        else:
+            #create it
+            self._name = new_name
+
+    def __repr__(self):
+        return f'<Coffee name={self.name} />'
 
 class Customer:
     def __init__(self, name):
         self.name = name
+    
+    @classmethod
+    def sum_coffee_orders(cls, coffee, customer):
+        #filter through orders for matching coffee/customer pairs
+        coffee_orders = [order for order in Order.all if order.coffee == coffee and order.customer == customer]
+        #sum above prices
+        return sum([order.price for order in coffee_orders])
+
+    @classmethod 
+    def most_aficionado(cls, coffee):
+        # all customer orders
+        # filter for matching coffee orders
+        coffee_orders = [order for order in Order.all if order.coffee == coffee]
+        if(len(coffee_orders) == 0):
+            return None
+        # sum orders per customer
+        # cf: current coffee order that max is iterating over
+        return(max(coffee_orders, key=lambda cf: Customer.sum_coffee_orders
+        (coffee, cf.customer))).customer
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        #order contains information regarding coffee 
+        return list(set([order.coffee for order in self.orders()]))
     
     def create_order(self, coffee, price):
-        pass
+        new_order = Order(self, coffee, price)
+        return new_order
+
+    @property 
+    def name(self):
+        return self._name 
+    
+    @name.setter 
+    def name(self, new_name):
+        if(isinstance(new_name, str) and 1 <= len(new_name) <= 15):
+            self._name = new_name 
+        else:
+            print('no')
+
+    def __repr__(self):
+        return f'<Customer name={self.name} />'
     
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property 
+    def price(self):
+        return self._price 
+    
+    @price.setter 
+    def price(self, new_price):
+        if(not hasattr(self, 'price') and isinstance(new_price, float) and 1.0 <= new_price <= 10.0):
+            self._price = new_price 
+        else:
+            print('no')
+
+    def __repr__(self):
+        return f'<Order customer={self.customer} coffe={self.coffee} price={self.price} />'
--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,23 @@ from classes.many_to_many import Order
 from classes.many_to_many import Coffee
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug")
+    mocha = Coffee('mocha')
+    latte = Coffee('latte')
+    espresso = Coffee('espresso')
+    decaf = Coffee('decaf')
+
+    tim = Customer('tim')
+    harry = Customer('harry')
+    leia = Customer('leia')
+    
+    
+
+    o1 = Order(tim, mocha, 3.0) # uses __init__ directly
+    o2 = Order(tim, mocha, 5.0)
+    o3 = Order(harry, decaf, 10.0)
+    o4 = Order(leia, latte, 10.0)
+    o5 = Order(leia, mocha, 3.0)
+    o6 = leia.create_order(mocha, 9.0) # goes through Customer instance
 
     ipdb.set_trace()
+
